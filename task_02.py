4da29857315cc522749ffb3407f1f19a22c0b41d
pinto921 <pinto921@hotmail.com>
2016-09-17 07:19:23 +0000

    task 2

--- task_02.py
+++ task_02.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This module does some pretty crazy math."""
 
+""" lets do some math"""
 
 import hamlet
+
+POSITIONAL = hamlet.crazy_math(4,100000, 8, 98) 
